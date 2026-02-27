adcf6732f703b803bbaa3b8eb2dbc93b9d1a1c60
Gsnail157 <GaryWG.0120@gmail.com>
2026-02-26 23:28:52 -0500

    removing from main branch

--- scripts/Scripts/libgen.py
+++
@@ -1,7 +0,0 @@
-#!/sbin/python3
-
-import os
-
-print("Libgen Textbook Downloader")
-md5 = input("Enter MD5 number: ")
-os.system(f"cd ~/go/bin/ ; ./libgen-cli download {md5} -o ~/Documents/Textbooks")
